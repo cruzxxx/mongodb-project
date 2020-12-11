a4c750c036e6183795b974ed881fb73c5da1f7fa
stefcruz <crzstefanie@gmail.com>
2020-12-11 13:09:59 +0000

    connect to mongodb through python

---
+++ mongo.py
@@ -0,0 +1,34 @@
+import os
+import pymongo
+if os.path.exists("env.py"):
+    import env
+
+
+# const in python written in CAPITALS
+MONGO_URI = os.environ.get("MONGO_URI")
+DATABASE = "myFirstDB"
+COLLECTION = "celebrities"
+
+
+# function to call the connection to mongodb
+def mongo_connect(url):
+    try:
+        conn = pymongo.MongoClient(url)
+        print("Mongo is connected")
+        return conn
+    except pymongo.errors.ConnectionFailure as e:
+        print("Could not connect to MongoDB %s") % e
+
+
+# call connection function
+conn = mongo_connect(MONGO_URI)
+
+# set collection name to a variable
+coll = conn[DATABASE][COLLECTION]
+
+# find all items in the celebrities collection, it will return a mongodb object called "Cursor"
+documents = coll.find()
+
+# iterate through data
+for doc in documents:
+    print(doc)
